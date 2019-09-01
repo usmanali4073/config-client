c108f18747c6425340f2b4f9c77d9edeed0baad1
Usman <usmanali4073@gmail.com>
2019-09-01 00:38:50 -0400

    Working Client

---
+++ src/main/java/com/speaya/configclient/clientController.java
@@ -0,0 +1,21 @@
+package com.speaya.configclient;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RefreshScope
+public class clientController {
+
+    @Value("${msg:Default Message}")
+    private String message;
+    @GetMapping
+    public String GetCurrentEnv(){
+        return message;
+    }
+}
